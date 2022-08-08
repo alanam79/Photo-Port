fa7abc393ec1cacc52b4cb2c13d268674039ceaf
Alana <halo2382001@yahoo.com>
2022-08-07 20:54:21 -0400

    developed about and nav components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // You need to import React in every component file.
 // You can also import any images or CSS you want to use at the top.
-import React from 'react';
-import About from './components/About';
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
 
 function App() {
-
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
